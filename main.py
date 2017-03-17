59ee15f7cc17e85ef710088cec831302094dc419
Sophia <sakr@isir.upmc.fr>
2017-03-17 18:54:20 +0100

    texture+déformation

--- main.py
+++ main.py
@@ -13,17 +13,6 @@ OLDMUR = {MUR[0]: 0, MUR[1]: 0, MUR[2]: 0}
 
 #####################################################################################################
 
-VERTEX = """
-  uniform float scale;
-  attribute vec2 position;
-  attribute vec4 color;
-  varying vec4 v_color;
-  void main()
-  {
-    gl_Position = vec4(scale*position, 0.0, 1.0);
-    v_color = color;
-  } """
-
 VERTEXM = """
   uniform float depla;
   uniform float scale;
@@ -43,8 +32,8 @@ FRAGMENT = """
       gl_FragColor = v_color;
   } """
 
-# Pour les Textures 2D #
 ##################################################################################
+# Pour les Textures 2D #
 vertexX = """
 	uniform float scale;
     attribute vec2 position;
@@ -60,18 +49,34 @@ vertexX = """
 fragmentX = """
     uniform sampler2D texture;
     varying vec2 v_texcoord;
-    void main()
-    {
-        gl_FragColor = texture2D(texture, v_texcoord);
-    }
-"""
-##################################################################################
 
+   // uniform float time;
+   // uniform vec2 resolution;
+
+    void main( void )
+    {
+      //  vec2 pos = ( gl_FragCoord.xy / resolution.xy ) * 26.0 - 13.0;
+       //float x = sin(time + length(pos.xy)) + cos(10.0 + pos.x);
+       //float y = cos(time + length(pos.xy)) + sin(10.0+ pos.y);
+       //float v_texcoordX = v_texcoord[0]*x;
+      // float v_texcoordY = v_texcoord[1]*y;
+       //v_texcoord = [v_texcoordX,v_texcoordY]
+
+
+       gl_FragColor = texture2D(texture,v_texcoord);  // [ v_texcoordX*x, v_texcoordY*x]); //vec8( x * 0.5, y * 0.5, x * y, 1.0 ,texture2D(texture, v_texcoord);
+    }
+
+
+    //void main()
+    //{
+    //    gl_FragColor = texture2D(texture, v_texcoord);
+    //}
+"""
 
 #####################################################################################################
 
 # Build the program and corresponding buffers (with 4 vertices)
-FOND = gloo.Program(VERTEX, FRAGMENT, count=4)
+FOND = gloo.Program(VERTEXM, FRAGMENT, count=4)
 
 # Upload data into GPU
 FOND['color'] = [(1, 1, 1, 1)] * 4
@@ -115,27 +120,51 @@ def posmur(pos_mur, pos_aig=0):
 
 
 ##################################################################################    
+def sticky2(pos_mur,ord_text, pos_aig):
+    """
+    Compare la position du mur et de l'aiguille et retourne la déformation
+    """
+    dif = pos_aig-ord_text
+    posi = 0
+    if OLDMUR[pos_mur] == 1:
+        if dif < -0.1:
+            posi = 0
+            OLDMUR[pos_mur] = 0
+        elif dif-0.1 < 0:
+            posi = dif-0.1
+    else:
+        if dif > 0.1:
+            posi = 0
+            OLDMUR[pos_mur] = 1
+        elif dif > 0:
+            posi = dif
+    return posi
+
+def posmur2(pos_mur, ord_text, pos_aig=0):
+    """
+    retourne la liste correspondant aux coordonnées des triangles du mur
+    """
+    return [(ord_text, 0.375), (ord_text, 0), (ord_text+sticky2(pos_mur,ord_text, pos_aig), -0.5),\
+    (+1, 0),(+1, +1), (ord_text+sticky2(pos_mur,ord_text, pos_aig), 0.5), (ord_text, 1), (ord_text, 0.625)]
+
+
+##################################################################################    
 
 MUR1 = gloo.Program(vertexX, fragmentX, count=8)
 MUR1['position'] = posmur(MUR[0])
-MUR1['texcoord'] = [(0.25,0.375),(0.25,0),(0.25,0.5),(1,0),(1,1),(0.25,0.5),(0.25,1),(0.25,0.625)]
+MUR1['texcoord'] = [(0.25,0.375),(0,0),(0.25,0.5),(1,0),(1,1),(0.25,0.5),(0,1),(0.25,0.625)]
 MUR1['texture'] = imageio.imread("miel1.png")
 MUR1['scale'] = 1
-"""MUR1['texture'] = imageio.imread("miel1.png")
-MUR1['u_model'] = np.eye(4, dtype=np.float32)
-MUR1['u_view'] = glm.translation(0, 0, 0)
-MUR1['position'] = posmur(MUR[0])
-MUR1['scale'] = 1"""
 
 MUR2 = gloo.Program(vertexX, fragmentX, count=8)
 MUR2['position'] = posmur(MUR[1])
-MUR2['texcoord'] = [(0.5,0.375),(0.5,0),(0.5,0.5),(1,0),(1,1),(0.5,0.5),(0.5,1),(0.5,0.625)]
-MUR2['texture'] = imageio.imread("eau.png")
+MUR2['texcoord'] = [(0.5,0.375),(0,0),(0.5,0.5),(1,0),(1,1),(0.5,0.5),(0,1),(0.5,0.625)]
+MUR2['texture'] = imageio.imread("bio4.png")
 MUR2['scale'] = 1
 
 MUR3 = gloo.Program(vertexX, fragmentX, count=8)
 MUR3['position'] = posmur(MUR[2])
-MUR3['texcoord'] = [(0.75,0.375),(0.75,0),(0.75,0.5),(1,0),(1,1),(0.75,0.5),(0.75,1),(0.75,0.625)]
+MUR3['texcoord'] = [(0.75,0.375),(0,0),(0.75,0.5),(1,0),(1,1),(0.75,0.5),(0,1),(0.75,0.625)]
 MUR3['texture'] = imageio.imread("sable.png")
 MUR3['scale'] = 1
 
@@ -145,6 +174,7 @@ print('position mur 2 :')
 print(posmur(MUR[1]))
 print('position mur 3 :')
 print(posmur(MUR[2]))"""
+
 #####################################################################################################
 # Create a window with a valid GL context
 WINDOW = app.Window(800, 600)
@@ -162,15 +192,15 @@ def on_draw(dtemps):
     AIGUILLE["depla"] += dtemps
     depla = math.sin(AIGUILLE["depla"])
     MUR1['position'] = posmur(MUR[0], depla)
-    MUR1['texcoord'] = [(0.25,0.375),(0.25,0),(0.25+sticky(MUR[0], depla),0.5),(1,0),(1,1),(0.25+sticky(MUR[0], depla),0.5),(0.25,1),(0.25,0.625)]
+    MUR1['texcoord'] = [(0.25,0.375),(0,0),(0.25,0.5),(1,0),(1,1),(0.25,0.5),(0,1),(0.25,0.625)]
     #print(posmur(MUR[0], depla))
     MUR1.draw(gl.GL_TRIANGLE_STRIP)
     MUR2['position'] = posmur(MUR[1], depla)
-    MUR1['texcoord'] = [(0.5,0.375),(0.5,0),(0.5+sticky(MUR[1], depla),0.5),(1,0),(1,1),(0.5+sticky(MUR[1], depla),0.5),(0.5,1),(0.5,0.625)]
+    MUR1['texcoord'] = [(0.5,0.375),(0,0),(0.5,0.5),(1,0),(1,1),(0.5,0.5),(0,1),(0.5,0.625)]
     #print(posmur(MUR[1], depla))
     MUR2.draw(gl.GL_TRIANGLE_STRIP)
     MUR3['position'] = posmur(MUR[2], depla)
-    MUR1['texcoord'] = [(0.75,0.375),(0.75,0),(0.75+sticky(MUR[2], depla),0.5),(1,0),(1,1),(0.75+sticky(MUR[2], depla),0.5),(0.75,1),(0.75,0.625)]
+    MUR1['texcoord'] = [(0.75,0.375),(0,0),(0.75,0.5),(1,0),(1,1),(0.75,0.5),(0,1),(0.75,0.625)]
     #print(posmur(MUR[2], depla))
     MUR3.draw(gl.GL_TRIANGLE_STRIP)
     AIGUILLE['position'] = [(-4, -0.05), (-4, +0), (-0.6+depla+0.5, -0.05), (-0.5+depla+0.5, +0)]
