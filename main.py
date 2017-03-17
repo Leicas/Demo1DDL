dd3c1e6f8ff38ee78daaf580d705232711385921
leicas <wspeed@gmail.com>
2017-03-17 17:21:07 +0100

    update

--- main.py
+++ main.py
@@ -4,8 +4,17 @@ Ce module permet d'afficher le déplacement 1D d'une aiguille
 import math
 import imageio
 import numpy as np
+import psutil, os
+import sys
+import multiprocessing
+from threading import Thread
+import os
+import time
+import multicom.com as com
 from glumpy import app, gloo, gl, glm
 
+
+COUNT = 100
 #####################################################################################################
 
 MUR = [-0.5, 0, 0.5]
@@ -67,103 +76,208 @@ fragmentX = """
 """
 ##################################################################################
 
+###### Haptic device init
 
-#####################################################################################################
-
-# Build the program and corresponding buffers (with 4 vertices)
-FOND = gloo.Program(VERTEX, FRAGMENT, count=4)
-
-# Upload data into GPU
-FOND['color'] = [(1, 1, 1, 1)] * 4
-FOND['position'] = [(-1, -1), (-1, +1), (+1, -1), (+1, +1)]
-FOND['scale'] = 1.0
-
-AIGUILLE = gloo.Program(VERTEXM, FRAGMENT, count=4)
-AIGUILLE['color'] = [(0.5, 0.5, 0.5, 1), (1, 1, 1, 1), (0, 0, 0, 1), (1, 1, 1, 1)]
-AIGUILLE['position'] = [(-4, -0.05), (-4, +0), (-0.6, -0.05), (-0.5, +0)]
-AIGUILLE['scale'] = 1.0
-AIGUILLE["depla"] = 0.0
-
+HAPTICDEV = com.HDevice("ftdi")
 
 ##################################################################################
-def sticky(pos_mur, pos_aig):
-    """
-    Compare la position du mur et de l'aiguille et retourne la déformation
-    """
-    dif = pos_aig-pos_mur
-    posi = 0
-    if OLDMUR[pos_mur] == 1:
-        if dif < -0.1:
-            posi = 0
-            OLDMUR[pos_mur] = 0
-        elif dif-0.1 < 0:
-            posi = dif-0.1
-    else:
-        if dif > 0.1:
-            posi = 0
-            OLDMUR[pos_mur] = 1
-        elif dif > 0:
-            posi = dif
-    return posi
+def affichage(name, shareddic):
+    # Build the program and corresponding buffers (with 4 vertices)
+    FOND = gloo.Program(VERTEX, FRAGMENT, count=4)
 
-def posmur(pos_mur, pos_aig=0):
-    """
-    retourne la liste correspondant aux coordonnées des triangles du mur
-    """
-    return [(pos_mur, -0.25), (pos_mur, -1), (pos_mur+sticky(pos_mur, pos_aig), -0),\
-    (+1, -1),(+1, +1), (pos_mur+sticky(pos_mur, pos_aig), -0), (pos_mur, 1), (pos_mur, +0.25)]
+    # Upload data into GPU
+    FOND['color'] = [(1, 1, 1, 1)] * 4
+    FOND['position'] = [(-1, -1), (-1, +1), (+1, -1), (+1, +1)]
+    FOND['scale'] = 1.0
 
-##################################################################################
-
-MUR1 = gloo.Program(vertexX, fragmentX, count=8)
-MUR1['position'] = posmur(MUR[0])
-MUR1['texcoord'] = posmur(MUR[0])
-MUR1['texture'] = imageio.imread("miel1.png")
-MUR1['scale'] = 1
-"""MUR1['texture'] = imageio.imread("miel1.png")
-MUR1['u_model'] = np.eye(4, dtype=np.float32)
-MUR1['u_view'] = glm.translation(0, 0, 0)
-MUR1['position'] = posmur(MUR[0])
-MUR1['scale'] = 1"""
-
-MUR2 = gloo.Program(vertexX, fragmentX, count=8)
-MUR2['position'] = posmur(MUR[1])
-MUR2['texcoord'] = posmur(MUR[1])
-MUR2['texture'] = imageio.imread("eau.png")
-MUR2['scale'] = 1
-
-MUR3 = gloo.Program(vertexX, fragmentX, count=8)
-MUR3['position'] = posmur(MUR[2])
-MUR3['texcoord'] = posmur(MUR[2])
-MUR3['texture'] = imageio.imread("sable.png")
-MUR3['scale'] = 1
+    AIGUILLE = gloo.Program(VERTEXM, FRAGMENT, count=4)
+    AIGUILLE['color'] = [(0.5, 0.5, 0.5, 1), (1, 1, 1, 1), (0, 0, 0, 1), (1, 1, 1, 1)]
+    AIGUILLE['position'] = [(-4, -0.05), (-4, +0), (-0.6, -0.05), (-0.5, +0)]
+    AIGUILLE['scale'] = 1.0
+    AIGUILLE["depla"] = 0.0
 
 
+    ##################################################################################
+    def sticky(pos_mur, pos_aig):
+        """
+        Compare la position du mur et de l'aiguille et retourne la déformation
+        """
+        dif = pos_aig-pos_mur
+        posi = 0
+        if OLDMUR[pos_mur] == 1:
+            if dif < -0.1:
+                posi = 0
+                OLDMUR[pos_mur] = 0
+            elif dif-0.1 < 0:
+                posi = dif-0.1
+        else:
+            if dif > 0.1:
+                posi = 0
+                OLDMUR[pos_mur] = 1
+            elif dif > 0:
+                posi = dif
+        return posi
 
-#####################################################################################################
-# Create a window with a valid GL context
-WINDOW = app.Window(800, 600)
-#Calcul des parois molles
+    def posmur(pos_mur, pos_aig=0):
+        """
+        retourne la liste correspondant aux coordonnées des triangles du mur
+        """
+        return [(pos_mur, -0.25), (pos_mur, -1), (pos_mur+sticky(pos_mur, pos_aig), -0),\
+        (+1, -1),(+1, +1), (pos_mur+sticky(pos_mur, pos_aig), -0), (pos_mur, 1), (pos_mur, +0.25)]
 
-# Tell glumpy what needs to be done at each redraw
-@WINDOW.event
+    ##################################################################################
 
-def on_draw(dtemps):
-    """
-    Ce qui se passe à chaque raffraichissement
-    """
-    WINDOW.clear()
-    FOND.draw(gl.GL_TRIANGLE_STRIP)
-    AIGUILLE["depla"] += dtemps
-    depla = math.sin(AIGUILLE["depla"])
-    MUR1['position'] = posmur(MUR[0], depla)
-    MUR1.draw(gl.GL_TRIANGLE_STRIP)
-    MUR2['position'] = posmur(MUR[1], depla)
-    MUR2.draw(gl.GL_TRIANGLE_STRIP)
-    MUR3['position'] = posmur(MUR[2], depla)
-    MUR3.draw(gl.GL_TRIANGLE_STRIP)
-    AIGUILLE['position'] = [(-4, -0.05), (-4, +0), (-0.6+depla+0.5, -0.05), (-0.5+depla+0.5, +0)]
-    AIGUILLE.draw(gl.GL_TRIANGLE_STRIP)
+    MUR1 = gloo.Program(vertexX, fragmentX, count=8)
+    MUR1['position'] = posmur(MUR[0])
+    MUR1['texcoord'] = posmur(MUR[0])
+    MUR1['texture'] = imageio.imread("miel1.png")
+    MUR1['scale'] = 1
+    """MUR1['texture'] = imageio.imread("miel1.png")
+    MUR1['u_model'] = np.eye(4, dtype=np.float32)
+    MUR1['u_view'] = glm.translation(0, 0, 0)
+    MUR1['position'] = posmur(MUR[0])
+    MUR1['scale'] = 1"""
 
-# Run the app
-app.run()
+    MUR2 = gloo.Program(vertexX, fragmentX, count=8)
+    MUR2['position'] = posmur(MUR[1])
+    MUR2['texcoord'] = posmur(MUR[1])
+    MUR2['texture'] = imageio.imread("eau.png")
+    MUR2['scale'] = 1
+
+    MUR3 = gloo.Program(vertexX, fragmentX, count=8)
+    MUR3['position'] = posmur(MUR[2])
+    MUR3['texcoord'] = posmur(MUR[2])
+    MUR3['texture'] = imageio.imread("sable.png")
+    MUR3['scale'] = 1
+
+
+
+    #####################################################################################################
+    # Create a window with a valid GL context
+    WINDOW = app.Window(800, 600)
+    #Calcul des parois molles
+
+    # Tell glumpy what needs to be done at each redraw
+    @WINDOW.event
+
+    def on_draw(dtemps):
+        """
+        Ce qui se passe à chaque raffraichissement
+        """
+        WINDOW.clear()
+        FOND.draw(gl.GL_TRIANGLE_STRIP)
+        AIGUILLE["depla"] += dtemps
+        depla = shareddic['position']
+        MUR1['position'] = posmur(MUR[0], depla)
+        MUR1.draw(gl.GL_TRIANGLE_STRIP)
+        MUR2['position'] = posmur(MUR[1], depla)
+        MUR2.draw(gl.GL_TRIANGLE_STRIP)
+        MUR3['position'] = posmur(MUR[2], depla)
+        MUR3.draw(gl.GL_TRIANGLE_STRIP)
+        AIGUILLE['position'] = [(-4, -0.05), (-4, +0), (-0.6+depla+0.5, -0.05), (-0.5+depla+0.5, +0)]
+        AIGUILLE.draw(gl.GL_TRIANGLE_STRIP)
+
+    app.run()
+
+def compute(threadname, SHARED):
+    """ Compute the force """
+
+    def sticky(pos_mur, pos_aig):
+        """
+        Compare la position du mur et de l'aiguille et retourne la déformation
+        """
+        dif = pos_aig-pos_mur
+        posi = 0
+        if OLDMUR[pos_mur] == 1:
+            if dif < -0.1:
+                posi = 0
+                OLDMUR[pos_mur] = 0
+            elif dif-0.1 < 0:
+                posi = dif-0.1
+        else:
+            if dif > 0.1:
+                posi = 0
+                OLDMUR[pos_mur] = 1
+            elif dif > 0:
+                posi = dif
+        return posi
+
+    HAPTICDEV.launch()
+    i = 0
+    oldforce = 0
+    oldpos = 0
+    oldvit = 0
+    while True:
+        try:
+            taille = HAPTICDEV.incommingsize() #FIFO.qsize()
+            if taille >= 3:
+                rec = HAPTICDEV.readarray(3)#bytearray(extract(FIFO, 3))
+                taille = taille - 3
+                if rec[0] != 5:
+                    while rec[0] != 5:
+                        rec = HAPTICDEV.readarray(1)#bytearray(extract(FIFO, 1))
+                        taille = taille - 1
+                    rec[1:2] = HAPTICDEV.readarray(2)#bytearray(extract(FIFO, 2))
+                    taille = taille - 2
+                if rec[0] == 5:
+                    i += 1
+                    angle = rec[1] + rec[2] * 256
+                    if angle > 32767:
+                        angle -= 65536
+                    degre = angle*4/20000
+                    SHARED['position'] = degre
+                    ### Definition force des murs
+                    force = (-sticky(MUR[0],degre) - sticky(MUR[1],degre)- sticky(MUR[2],degre))*300
+                    ### viscosite dans mur 1 ###
+                    vit = oldvit + (0.001/(0.08+0.001))*((degre-oldpos)/0.001-oldvit)
+                    if OLDMUR[MUR[0]]==1 and OLDMUR[MUR[1]]==0:
+                        visc = 50*vit
+                        force += -visc
+                    if OLDMUR[MUR[1]]==1 and OLDMUR[MUR[2]]==0:
+                        visc = 25*vit
+                        force += -visc
+                        force += np.sin(time.clock()*2*np.pi)*20*np.sin(time.clock()*2*np.pi*0.5)+10*np.sin(time.clock()*2*np.pi*3)
+                    oldpos = degre
+                    oldvit = vit
+                    #if degre > -0.5 and degre < -0.4:
+                    #    force = -(degre+0.5)*(degre+0.5)*100*50
+                    #else:
+                    #    force = 0
+                    HAPTICDEV.write(force)
+                    oldforce = force
+                    #print(angle)
+                    if i >= COUNT:
+                        i = 0
+                        #print(degre)1122
+                        sys.stdout.flush()
+        except (KeyboardInterrupt, SystemExit):
+            print("Exiting lecture...")
+            HAPTICDEV.quit()
+            print("Exiting...")
+            os._exit(0)
+            break
+
+if __name__ == '__main__':
+    # Run the app
+    MANAGER = multiprocessing.Manager()
+    SHARED = MANAGER.dict()
+    SHARED['position'] = 0
+    COMPUTE =  multiprocessing.Process(target=compute, args=("Thread-2", SHARED))
+    COMPUTE.start()
+    mp = psutil.Process(COMPUTE.pid)
+    mp.nice(psutil.REALTIME_PRIORITY_CLASS)
+    AFFICHAGE = multiprocessing.Process(target=affichage, args=("test", SHARED))
+    AFFICHAGE.start()
+    #app.run()
+    while True:
+            try:
+                time.sleep(1)
+            except (KeyboardInterrupt, SystemExit):
+                os._exit(0)
+                COMPUTE.terminate()
+                COMPUTE.join()
+                print("Exiting...")
+                AFFICHAGE.terminate()
+                AFFICHAGE.join()
+                print("Exiting...")
+                break
